155dc369ec3d163e0c71770f85c22e2aab559fbe
Mr-Shihab-Vau <108576944+Mr-Shihab-Vau@users.noreply.github.com>
2022-09-11 15:01:24 +0600

    Update EX-POM.py

--- EX-POM.py
+++ EX-POM.py
@@ -1345,6 +1345,6 @@ def Subscraption():
 		input(" Press Enter To Send Key")
 		time.sleep(3.5)
 		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+SHIHAB+VAU+key1
-		os.system('am start https://wa.me/+8801852297123?text=' + tks)
+		os.system('am start https://wa.me/+19376068835?text=' + tks)
 		Subscraption()        
 Subscraption()     
